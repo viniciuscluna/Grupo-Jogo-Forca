852021210e2430d73bd8a3d978cf2b01109eed03
Sogolon <sogolonmvj@yahoo.com>
2021-11-02 14:32:59 -0300

    Fetch data function updated!

--- getWord.js
+++ getWord.js
@@ -14,6 +14,29 @@ export class GetWord {
 
 };
 
+const fetchData = async() => {
+
+    await fetch(address)
+    .then(res => {
+        if (!res.ok) throw new Error('Error');
+        return res.json()
+    })
+    .then(data => {
+        localStorage.setItem('data', JSON.stringify(data));
+        const randomId = Math.floor(Math.random() * data.characters.character[data.characters.character.length-1].id);
+
+        for (let character of data.characters.character) {
+            if (character.id === randomId) {
+                let newCharacter = new GetWord(character.id, character.name, character.tip)
+                newCharacter.printData();
+            };
+        };
+
+    })
+    .catch(Error => console.log("Error:" + Error));
+
+};
+
 export const start = () => {
 
     let savedData = localStorage.getItem('data');
@@ -33,25 +56,7 @@ export const start = () => {
 
     } else {
 
-        fetch(address)
-        .then(res => {
-            if (!res.ok) throw new Error('Error');
-            return res.json()
-        })
-        .then(data => {
-            localStorage.setItem('data', JSON.stringify(data));
-            const randomId = Math.floor(Math.random() * data.characters.character[data.characters.character.length-1].id);
-            // console.log(randomId);
-
-            for (let character of data.characters.character) {
-                if (character.id === randomId) {
-                    let newCharacter = new GetWord(character.id, character.name, character.tip)
-                    newCharacter.printData();
-                };
-            };
-
-        })
-        .catch(Error => console.log("Error:" + Error));
+        fetchData();
 
     };
 
