5aa1b13a8e908b5e0031d488c0d7d635a7e09963
Sogolon <sogolonmvj@yahoo.com>
2021-11-02 14:12:18 -0300

    File created to get the words updated!

--- getWord.js
+++ getWord.js
@@ -1,7 +1,7 @@
 import { address } from './api.js';
 
-class GetWord {
-
+export class GetWord {
+    
     constructor(id, word, tip) {
         this.id = id;
         this.word = word;
@@ -14,21 +14,45 @@ class GetWord {
 
 };
 
-fetch(address)
-.then(res => {
-    if (!res.ok) throw new Error('Error');
-    return res.json()
-})
-.then(data => {
-    const randomId = Math.floor(Math.random() * data.characters.character[data.characters.character.length-1].id);
-    console.log(randomId);
+export const start = () => {
 
-    for (let i of data.characters.character) {
-        if (i.id === randomId) {
-            let newCharacter = new GetWord(i.id, i.name, i.tip)
-            newCharacter.printData();
+    let savedData = localStorage.getItem('data');
+
+    if (savedData) {
+
+        savedData = JSON.parse(savedData);
+
+        const randomId = Math.floor(Math.random() * savedData.characters.character[savedData.characters.character.length-1].id);
+
+        for (let character of savedData.characters.character) {
+            if (character.id === randomId) {
+                let newCharacter = new GetWord(character.id, character.name, character.tip)
+                newCharacter.printData();
+            };
         };
+
+    } else {
+
+        fetch(address)
+        .then(res => {
+            if (!res.ok) throw new Error('Error');
+            return res.json()
+        })
+        .then(data => {
+            localStorage.setItem('data', JSON.stringify(data));
+            const randomId = Math.floor(Math.random() * data.characters.character[data.characters.character.length-1].id);
+            // console.log(randomId);
+
+            for (let character of data.characters.character) {
+                if (character.id === randomId) {
+                    let newCharacter = new GetWord(character.id, character.name, character.tip)
+                    newCharacter.printData();
+                };
+            };
+
+        })
+        .catch(Error => console.log("Error:" + Error));
+
     };
 
-})
-.catch(Error => console.log("Error:" + Error));
+};
